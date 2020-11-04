4e91906cee82cd8936755117cb4b25ebde8e63f1
mrinmoy02 <mrinmoy.biswas215@outlook.com>
2020-11-05 01:23:55 +0530

    update

---
+++ src/app/fps/fps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fps',
+  templateUrl: './fps.component.html',
+  styleUrls: ['./fps.component.scss']
+})
+export class FpsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
